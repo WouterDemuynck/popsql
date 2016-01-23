7f5fbdc3376f50cfecdb9613bb9dfc994068d3dc
Wouter Demuynck <wouter@adamsoftware.net>
2016-01-23 09:43:06 +0100

    Initial commit.

---
+++ src/Popsql/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Popsql")]
+[assembly: AssemblyDescription("Popsql is a library eliminating the need for magic SQL strings in your code.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Popsql")]
+[assembly: AssemblyCopyright("Copyright © 2012-2016 Wouter Demuynck")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("4291f6a6-1640-4f12-a02f-a30ccabe3e5e")]
+
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyInformationalVersion("2.0.0")]
