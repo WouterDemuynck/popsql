84aa3b462d50ac1943744918d71ba86a586be92f
Wouter Demuynck <wouter.am.demuynck@gmail.com>
2016-05-06 08:39:58 +0200

    Updated build scripts.

--- build/version.ps1
+++ build/version.ps1
@@ -12,22 +12,6 @@ if(-not $Env:MajorVersion)
     exit 1
 }
 
-$commitId = git rev-parse --short HEAD
-$majorVersion = $Env:MajorVersion
-$minorVersion = $Env:MinorVersion
-$patchVersion = $Env:PatchVersion
-$prereleaseVersion = $Env:PrereleaseVersion
-
-$packageVersion = "$majorVersion.$minorVersion.$patchVersion"
-if (-not $prereleaseVersion -eq "")
-{
-	$packageVersion = "$packageVersion-$prereleaseVersion-$commitId"
-}
-
-Write-Host "##vso[task.setvariable variable=commitId;]$commitId"
-Write-Host "##vso[task.setvariable variable=packageVersion;]$packageVersion"
-
-# Update AssemblyInfo files.
 function Get-ScriptDirectory 
 {
 	Split-Path -parent $PSCommandPath
@@ -40,26 +24,54 @@ if (-not $src)
 	$src = "$src\..\src\"
 }
 
+# Get current commit hash.
+$commitId = git rev-parse --short HEAD
+$majorVersion = $Env:MajorVersion
+$minorVersion = $Env:MinorVersion
+$patchVersion = $Env:PatchVersion
+$prereleaseVersion = $Env:PrereleaseVersion
+
+# Auto-generate version numbers.
 $build = Get-ScriptDirectory
 $buildTools = Join-Path $build 'BuildTools.dll'
 Add-Type -Path $buildTools
+$generatedVersion = [BuildTools.VersionNumberGenerator]::GenerateVersion($majorVersion, $minorVersion, [BuildTools.BuildNumberType]::YearMonthDay, [BuildTools.RevisionNumberType]::HourMinute, $(Get-Date -Date "2012-09-29 00:00:00Z"))
+
+$packageVersion = "$majorVersion.$minorVersion.$patchVersion"
+if (-not $prereleaseVersion -eq "")
+{
+	$packageVersion = "$packageVersion-$prereleaseVersion-$($generatedVersion.Build)-$($generatedVersion.Revision)"
+}
 
 $assemblyVersion = "$majorVersion.$minorVersion.$patchVersion"
-$assemblyFileVersion = [BuildTools.VersionNumberGenerator]::GenerateVersion($majorVersion, $minorVersion, [BuildTools.BuildNumberType]::YearMonthDay, [BuildTools.RevisionNumberType]::HourMinute, $(Get-Date -Date "2012-09-29 00:00:00Z")).ToString()
+$assemblyFileVersion = $generatedVersion.ToString()
 $assemblyInfoVersion = "$packageVersion"
+$assemblyTitle = "Popsql $assemblyVersion (rev. $commitId)"
+$assemblyCopyright = "Copyright $([char]0x00A9) 2012-$([DateTime]::Now.Year) Wouter Demuynck"
 
+Write-Host "Assembly Title: $assemblyTitle"
+Write-Host "Assembly Copyright: $assemblyCopyright"
 Write-Host "Assembly Version: $assemblyVersion"
 Write-Host "Assembly File Version: $assemblyFileVersion"
 Write-Host "Assembly Informational Version: $assemblyInfoVersion"
 
+# Publish variables to VSTS.
+Write-Host "##vso[task.setvariable variable=packageVersion;]$packageVersion"
+Write-Host "##vso[task.setvariable variable=commitId;]$commitId"
+
+# Update assembly information.
 foreach ($file in (Get-ChildItem -path $src -include 'AssemblyInfo.cs' -recurse))
 {	
+	$assemblyTitlePattern = '(AssemblyTitle\()("[^"]*")(\))'
+	$assemblyCopyrightPattern = '(AssemblyCopyright\()("[^"]*")(\))'
 	$assemblyVersionPattern = '(AssemblyVersion\()("[^"]*")(\))'
 	$fileVersionPattern = '(AssemblyFileVersion\()("[^"]*")(\))'
 	$assemblyInfoPattern = '(AssemblyInformationalVersion\()("[^"]*")(\))'
 	
-	(Get-Content $file.FullName) -replace $assemblyVersionPattern, "`$1`"$assemblyVersion`"`$3" `
+	(Get-Content $file.FullName) -replace $assemblyTitlePattern, "`$1`"$assemblyTitle`"`$3" `
+								 -replace $assemblyCopyrightPattern, "`$1`"$assemblyCopyright`"`$3" `
+								 -replace $assemblyVersionPattern, "`$1`"$assemblyVersion`"`$3" `
 								 -replace $fileVersionPattern, "`$1`"$assemblyFileVersion`"`$3" `
 								 -replace $assemblyInfoPattern, "`$1`"$assemblyInfoVersion`"`$3" `
-		| Out-File $file.FullName -encoding UTF8 -force
+		| Out-File $file.FullName -encoding Unicode -force
 }
