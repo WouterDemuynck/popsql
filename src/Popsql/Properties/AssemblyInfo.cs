9ec606b0ce227f6ab79642b0a23f8a5dad002fa0
Wouter Demuynck <wouter.am.demuynck@gmail.com>
2017-06-10 12:29:38 +0200

    Ported Popsql to .NET Standard 1.6 (and also targeting .NET Framework 4.5.2 now)

--- src/Popsql/Properties/AssemblyInfo.cs
+++ src/Popsql/Properties/AssemblyInfo.cs
@@ -1,24 +1,5 @@
-﻿using System;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Popsql")]
-[assembly: AssemblyDescription("Popsql is a library eliminating the need for magic SQL strings in your code.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Popsql")]
-[assembly: AssemblyCopyright("Copyright © 2012-2016 Wouter Demuynck")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-[assembly: CLSCompliant(true)]
-[assembly: ComVisible(false)]
-[assembly: Guid("4291f6a6-1640-4f12-a02f-a30ccabe3e5e")]
-
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
-[assembly: AssemblyInformationalVersion("2.0.0")]
 
 [assembly: InternalsVisibleTo("Popsql.Tests, PublicKey=" +
 	"0024000004800000940000000602000000240000525341310004000001000100d75691bc241002" +
